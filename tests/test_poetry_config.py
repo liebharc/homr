a5810f3ef274971aa4c9c36d4b0acd98aabc9c86
Christian Liebhardt <christian.liebhardt@arcor.de>
2025-08-18 19:39:43 +0200

    Removed test as it doesn't support the version range syntax

--- tests/test_poetry_config.py
+++ tests/test_poetry_config.py
@@ -1,15 +1,9 @@
 import pathlib
-import re
-from platform import python_version
-
-CURRENT_MINOR_VERSION = ".".join(python_version().split(".")[:2])
 
 TEST_FILE_PATH = pathlib.Path(__file__).parent.resolve()
 PYPROJECT_TOML_PATH = list(TEST_FILE_PATH.glob("../pyproject.toml"))
 MAKEFILE_PATH = list(TEST_FILE_PATH.glob("../Makefile"))
 PRECOMMIT_HOOKS_PATH = list(TEST_FILE_PATH.glob("../.git/hooks"))
-
-PYPROJECT_TOML_VERSION_REGEX = r"\n((?:py(?:thon)?)(?:[_-]version)?)\s=\D+(\d+.\d+)"
 
 
 def test_file_uniqueness() -> None:
@@ -24,19 +18,6 @@ def test_file_uniqueness() -> None:
         raise ValueError(
             f"Found more than one 'Makefile': {', '.join(str(p) for p in MAKEFILE_PATH) }"
         )
-
-
-def test_consistent_versioning() -> None:
-    with open(PYPROJECT_TOML_PATH[0], encoding="utf-8") as f:
-        pyproject_toml = f.read()
-
-    # TOML configurations
-    toml_versions = re.findall(PYPROJECT_TOML_VERSION_REGEX, pyproject_toml)
-    for var_name, var_version in toml_versions:
-        if var_version != CURRENT_MINOR_VERSION:
-            raise ValueError(
-                f'"{var_name}" on file pyproject.toml is not set to {CURRENT_MINOR_VERSION}'
-            )
 
 
 def test_isset_precommit_hooks() -> None:
